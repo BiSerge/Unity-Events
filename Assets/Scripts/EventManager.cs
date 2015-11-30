2769c28aaae5caeaed8c04fce3fc7cfd3824eb5b
Сергей <bichevoi@gmail.com>
2015-11-30 21:04:52 +0600

    11

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+    public delegate void ClickAction();
+    public static event ClickAction OnClicked;
+
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 5, 100, 30), "Click"))
+        {
+            if (OnClicked != null)
+                OnClicked();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
